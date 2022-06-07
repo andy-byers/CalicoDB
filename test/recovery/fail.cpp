d3089e4bdffe4ab23e607152050b4198a090272f
Andrew Byers <84697299+andy-byers@users.noreply.github.com>
2022-06-07 12:44:01 -0500

    Improvements (#6)
    * Added a CRC field to the file and node headers and fixed some bugs.
    
    * Added a component to encapsulate page updates and compress overlapping/touching ranges. This makes the WAL records way smaller and improves performance a decent amount.
    
    * Fixed some bugs.
    
    * Rearranged/added some tests. Still working on the in-memory buffer pool, which would provide a much faster, temporary database.
    
    * Added some simple benchmarks and reworked some components. Lots of style fixes.
    
    * Changed how cells are constructed.
    
    * Preparations for more proactive merges.
    
    * Got proactive B-tree merges working tentatively.
    
    * Changed the algorithm a bit. Now we attempt to merge nodes all the way up to the root, keeping the tree shorter overall.
    
    * Updated the README and got rid of some unnecessary #includes.
    
    * Working on CI.
    
    * Working on CI 2.
    
    * Working on CI 3.
    
    * Working on CI 4.
    
    * It seems obvious in retrospect, but I had no clue that one had to use the aligned overload of operator delete() when the data was allocated with aligned new. Thanks ASan!
    
    * Of course, I then immediately forget to do delete[].
    
    * CI
    
    * CI
    
    * CI
    
    * CI
    
    * Got some super basic fuzzing to work.
    
    * Don't build the fuzzers by default, not supported by some CI builds, apparently.
    
    * Made a test for database recovery (still needs automation and inclusion in CI) and worked on the README.

---
+++ test/recovery/fail.cpp
@@ -0,0 +1,80 @@
+
+#include <filesystem>
+#include <fstream>
+#include "cub/cub.h"
+#include "tools.h"
+
+#ifdef NDEBUG
+#  error "This test must run with assertions enabled"
+#endif
+
+using namespace cub;
+
+static constexpr Size KEY_WIDTH {6};
+static constexpr Size LIMIT {10'000'000};
+
+auto show_usage()
+{
+    std::cout << "usage: fail PATH N SEED\n";
+    std::cout << "  Parameters\n";
+    std::cout << "==============\n";
+    std::cout << "PATH: Path at which to create the database\n";
+    std::cout << "N: Number of records committed to the database\n";
+    std::cout << "SEED: Seed for generating the modifications\n";
+}
+
+auto main(int argc, const char *argv[]) -> int
+{
+    using namespace cub;
+    if (argc != 4) {
+        show_usage();
+        return 1;
+    }
+    const std::string path {argv[1]};
+    const auto value_path = path + "_values";
+    const auto num_committed = std::stoul(argv[2]);
+    const auto max_database_size = num_committed * 5;
+    Random random {static_cast<Random::Seed>(std::stoi(argv[3]))};
+
+    std::filesystem::remove(path);
+    std::filesystem::remove(get_wal_path(path));
+
+    // Use small pages and few frames to cause lots of stealing.
+    Options options;
+    options.page_size = 0x200;
+    options.block_size = 0x200;
+
+    auto db = Database::open(path, options);
+    {
+        std::ofstream ofs {value_path, std::ios::trunc};
+        CUB_EXPECT_TRUE(ofs.is_open());
+        for (Index key {}; key < num_committed; ++key) {
+            const auto k = make_key<KEY_WIDTH>(key);
+            const auto v = random_string(random, 2, 15);
+            db.insert(_b(k), _b(v));
+            ofs << v << '\n';
+        }
+    }
+
+    db.commit();
+    puts(value_path.c_str());
+
+    // Modify the database until we receive a signal or hit the operation limit.
+    for (Index i {}; i < LIMIT; ++i) {
+        const auto key = std::to_string(random.next_int(num_committed * 2));
+        const auto value = random_string(random, 0, options.page_size / 2);
+        db.insert(_b(key), _b(value));
+
+        // Keep the database from getting too large.
+        if (const auto info = db.get_info(); info.record_count() > max_database_size) {
+            while (info.record_count() >= max_database_size / 2) {
+                const auto record = db.lookup_minimum();
+                CUB_EXPECT_NE(record, std::nullopt);
+                db.remove(_b(record->key));
+            }
+        }
+    }
+    // We should never get here.
+    puts("<error>");
+    return 0;
+}
